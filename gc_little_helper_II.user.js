e5bef675dd9f54e38058ff5e553154201917385d
CachingFoX <cachingfox@mail-hauser.de>
2018-09-02 23:10:10 +0200

    #815 move code in the "New Dashboard" section

--- gc_little_helper_II.user.js
+++ gc_little_helper_II.user.js
@@ -5563,6 +5563,36 @@ var mainGC = function() {
             }
             // Change link "Your lists" from ".../account/lists" to ".../my/lists.aspx".
             if (settings_my_lists_old_fashioned) $('#DashboardSidebar ul li a[href*="/account/lists"]').prop("href", "/my/lists.aspx");
+
+            // add link to Ignore List into dashboard sidebar
+            if (settings_embedded_smartlink_ignorelist) {
+
+                function openIgnoreList(response) { 
+                    try {
+                        if (response.responseText) {
+                            var linkIgnoreList = $(response.responseText).find('a[href*="/bookmarks/view.aspx?code="]').first().attr('href');
+                            window.open(linkIgnoreList,"_self");
+                        }
+                    } catch(e) {gclh_error("function openIgnoreList()",e);}
+                }
+
+                var sidebarLists = $($('ul[class="link-block"] a[href*="/my/watchlist.aspx"]')[0]);
+                var html = '<li><a id="gclh_goto_ignorelist" href="#">Ignore List</a></li>';
+                sidebarLists.parent().after(html);
+
+                $("#gclh_goto_ignorelist").click( function(e) {
+                    try {
+                        // link to ignore list is not static, the id can be changed
+                        GM_xmlhttpRequest({
+                            method: "GET",
+                            url: "https://www.geocaching.com/account/lists",
+                            onload: openIgnoreList
+                        });
+                        e.preventDefault();
+                    } catch(e) {gclh_error("Request link to Ignore List (#gclh_goto_ignorelist)",e);}
+                });
+            }
+
             appendCssStyle(css);
         } catch(e) {gclh_error("Improve new dashboard:",e);}
     }
@@ -11411,36 +11441,6 @@ var mainGC = function() {
                 console.log('Autosync: Hash function was not successful:');
                 console.log(error);
             });
-    }
-
-// add link to Ignore List into dashboard sidebar 
-    if (settings_embedded_smartlink_ignorelist && is_page("dashboard")) {
-        try {
-            var sidebarLists = $($('ul[class="link-block"] a[href*="/my/watchlist.aspx"]')[0]);
-            var html = '<li><a id="gclh_goto_ignorelist" href="#">Ignore List</a></li>';
-            sidebarLists.parent().after(html);
-        } catch(e) {gclh_error("Add link to Ignore List at the sidebar of the dashboard",e);}
-
-        function openIgnoreList(response) {
-            try {
-                if (response.responseText) {
-                    var linkIgnoreList = $(response.responseText).find('a[href*="/bookmarks/view.aspx?code="]').first().attr('href');
-                    window.open(linkIgnoreList,"_self");
-                }
-            } catch(e) {gclh_error("function openIgnoreList()",e);}
-        }
-
-        $("#gclh_goto_ignorelist").click( function(e) {
-            try {
-                // link to ignore list is not static, the id can be changed
-                GM_xmlhttpRequest({
-                    method: "GET",
-                    url: "https://www.geocaching.com/account/lists",
-                    onload: openIgnoreList
-                });
-                e.preventDefault();
-            } catch(e) {gclh_error("Request link to Ignore List",e);}
-        });
     }
 
 };  // End of mainGC.
